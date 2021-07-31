e81d25becf600e9c12423433d36c484f1e7a7ff1
Alexandr Drachinin <20sagebrush07@gmail.com>
2021-07-31 11:09:56 +0300

    Added new methods

--- school_hw_1.py
+++ school_hw_1.py
@@ -7,8 +7,31 @@ def average_grade(person):
             sum_grades += i
     return round((sum_grades / len_values), 1)
 
+def average_grade_students(students, course):
+    average_grades = 0
+    student_id = 0
+    for student in students:
+        if not isinstance(student, Student) and course in student.courses_in_progress and course in student.finished_courses:
+            print('Не студенты')
+            return
+        student_id += 1
+        average_grades += average_grade(student)
+        return round(average_grades / student_id, 1)
+
+def average_grade_lecturers(lecturers, course) :
+    average_grades = 0
+    lecturer_id = 0
+    for lecturer in lecturers:
+        if not isinstance(lecturer, Lecturer) and course in lecturer.courses_attached:
+            print('Не студенты')
+            return
+        lecturer_id += 1
+        average_grades += average_grade(lecturer)
+        return round(average_grades / lecturer_id, 1)
+
+
 class Student:
-    
+
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -32,6 +55,17 @@ class Student:
             f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
             f'Завершенные курсы: {", ".join(self.finished_courses)}')
         return res
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Не студент!')
+            return
+        if average_grade(self) > average_grade(other):
+            print(f'У студента {self.name} средняя оценка выше')
+            return
+        else:
+            print(f'У студента {other.name} средняя оценка выше')
+
         
 class Mentor:
 
@@ -70,15 +104,42 @@ class Lecturer(Mentor):
             f'Средняя оценка за лекции: {average_grade(self)}')
         return res
 
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Не лектор!')
+            return
+        if average_grade(self) > average_grade(other):
+            print(f'У лектора {self.name} средняя оценка выше')
+            return
+        else:
+            print(f'У лектора {other.name} средняя оценка выше')
+
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
- 
-cool_reviewer = Reviewer('Some', 'Buddy')
-cool_reviewer.courses_attached += ['Python']
+
+bad_student = Student('Name', 'Surname', 'Gender')
+bad_student.courses_in_progress += ['Python']
 
 cool_lecturer = Lecturer('John', 'Cena')
 cool_lecturer.courses_attached += ['Python']
+
+bad_lecturer = Lecturer('Michael', 'Jackson')
+bad_lecturer.courses_attached += ['Python']
+
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+bad_reviewer = Reviewer('Bad', 'Reviewer')
+bad_reviewer.courses_attached += ['Python']
+
+cool_reviewer.rate_hw(bad_student, 'Python', 3)
+cool_reviewer.rate_hw(bad_student, 'Python', 1)
+cool_reviewer.rate_hw(bad_student, 'Python', 2)
+
+best_student.rate_lc(bad_lecturer, 'Python', 1)
+best_student.rate_lc(bad_lecturer, 'Python', 2)
+best_student.rate_lc(bad_lecturer, 'Python', 1)
 
 best_student.rate_lc(cool_lecturer, 'Python', 10)
 best_student.rate_lc(cool_lecturer, 'Python', 5)
@@ -93,4 +154,8 @@ print(best_student)
 print(cool_reviewer)
 
 print(cool_lecturer)
+
+bad_lecturer > cool_lecturer
+
+bad_student > best_student
 
